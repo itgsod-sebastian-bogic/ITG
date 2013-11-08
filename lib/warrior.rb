31da5b373901e4869b7b45d6d92fb0567806d8c7
itgsod-sebastian-bogic <sebastian.bogic@elev.it-gymnasiet.se>
2013-11-08 10:48:47 +0100

    Added hp and weapon

--- lib/warrior.rb
+++ lib/warrior.rb
@@ -1,10 +1,49 @@
 class Warrior
 
-  attr_reader :name, :weapon
+  attr_reader :name
 
-  def initialize(name, weapon)
+  def initialize(name)
     @name = name
-    @weapon = weapon
+  end
+
+  def hp
+    raise NotImplementedError
+  end
+
+  def weapon
+    raise NotImplementedError
+  end
+
+end
+
+class SpearWarrior < Warrior
+
+  def initialize(name)
+    super(name)
+  end
+
+  def weapon
+    'Spear'
+  end
+
+  def hp
+    10
+  end
+
+end
+
+class SwordWarrior < Warrior
+
+  def initialize(name)
+    super(name)
+  end
+
+  def weapon
+    'Sword'
+  end
+
+  def hp
+    15
   end
 
 end
--- test/warrior_spec.rb
+++ test/warrior_spec.rb
@@ -4,13 +4,51 @@ require_relative '../lib/warrior'
 describe 'Warrior' do
 
   it 'should have a name and a weapon' do
-    w = Warrior.new('Baldriq', 'Sword')
-    w.weapon.should == 'Sword'
+    w = Warrior.new('Baldriq')
     w.name.should == 'Baldriq'
 
-    w = Warrior.new('Inigo', 'Hammer')
-    w.weapon.should == 'Hammer'
+    w = Warrior.new('Inigo')
     w.name.should == 'Inigo'
-
   end
+
+  it 'should not have a weapon' do
+    w = Warrior.new('')
+    expect { w.weapon }.to raise_error NotImplementedError
+  end
+
+  it 'should not have hp' do
+    w = Warrior.new('')
+    expect { w.hp }.to raise_error NotImplementedError
+  end
+
+end
+
+describe 'SpearWarrior' do
+
+  it 'should have a spear' do
+    sw = SpearWarrior.new('Inigo')
+    sw.name.should == 'Inigo'
+    sw.weapon.should == 'Spear'
+  end
+
+  it 'should have 10 hp' do
+    sw = SpearWarrior.new('')
+    sw.hp.should == 10
+  end
+
+end
+
+describe 'SwordWarrior' do
+
+  it 'should have a name and a sword' do
+    sw = SwordWarrior.new('Inigo')
+    sw.name.should == 'Inigo'
+    sw.weapon.should == 'Sword'
+  end
+
+  it 'should have 15 hp' do
+    sw = SwordWarrior.new('')
+    sw.hp.should == 15
+  end
+
 end
